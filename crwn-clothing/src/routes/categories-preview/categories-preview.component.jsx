a4bc15250185a56deb89ebacc9a7edce10abd439
Markéta Šlancarová <marketaslancarova@gmail.com>
2023-06-03 12:48:42 +0200

    added Firebase Database Storage

---
+++ crwn-clothing/src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,19 @@
+
+import { CategoriesContext } from "../../contexts/categories.context";
+import { useContext } from "react";
+
+import CategoryPreview from "../../components/category-preview/category.preview.component";
+
+const CategoriesPreview = () => {
+    const {categoriesMap} = useContext(CategoriesContext);
+    return (
+        <div className="category-preview-container">
+            {Object.keys(categoriesMap).map(title => {
+                const products = categoriesMap[title];
+                return <CategoryPreview key={title} title={title} products={products}/>
+            })}
+            </div>
+    );
+};
+
+export default CategoriesPreview;
