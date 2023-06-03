7ea708b96ed26914105dce6a8262fc2089873b0d
Markéta Šlancarová <marketaslancarova@gmail.com>
2023-06-03 09:31:33 +0200

    finished checkout page and checkout items

--- crwn-clothing/src/contexts/cart.context.jsx
+++ crwn-clothing/src/contexts/cart.context.jsx
@@ -1,5 +1,9 @@
 import { createContext,useEffect,useState } from "react";
 
+const clearCartItem = (cartItems, cartItemToRemove) => {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+}
+
 const addCartItem = (cartItems, productToAdd) => {
     const existingCartItem = cartItems.find(
         (cartItem) => cartItem.id === productToAdd.id
@@ -17,7 +21,9 @@ export const CartContext = createContext({
     cartItems: [],
     addItemToCart: () => {},
     removeItemFromCart: () => {},
+    clearItemFromCart: () => {},
     cartCount: 0,
+    total: 0
 
 })
 
@@ -37,6 +43,7 @@ export const CartProvider = ({children}) => {
     const [isCartOpen,setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [cartCount,setCartCount] = useState(0);
+    const [total,setCartTotal] = useState(0);
 
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems,productToAdd));
@@ -46,12 +53,22 @@ export const CartProvider = ({children}) => {
         setCartItems(removeCartItem(cartItems,productToRemove));
     }
 
+    const clearItemFromCart = (cartItemToRemove) => {
+        setCartItems(clearCartItem(cartItems,cartItemToRemove));
+    }
+
     useEffect(()=> {
         const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity,0);
         setCartCount(newCartCount);
     },[cartItems])
 
+    
+    useEffect(()=> {
+        const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity*cartItem.price,0);
+        setCartTotal(newCartTotal);
+    },[cartItems])
 
-    const value = {isCartOpen,setIsCartOpen, addItemToCart, cartItems, cartCount, removeItemFromCart}
+
+    const value = {isCartOpen,setIsCartOpen, addItemToCart, cartItems, cartCount, removeItemFromCart, clearItemFromCart,total}
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
--- crwn-clothing/src/routes/checkout/checkout.component.jsx
+++ crwn-clothing/src/routes/checkout/checkout.component.jsx
@@ -1,28 +1,51 @@
 
 import { CartContext } from '../../contexts/cart.context';
-
-import './checkout.style.scss'
 import { useContext } from 'react';
 
-const Checkout = () => {
-    const {cartItems, addItemToCart, removeItemFromCart} = useContext(CartContext);
+import CheckoutItem from '../../components/checkout-item/checkout-item.components';
 
-    return <div>
-        <h1> I am checkout page</h1>
-        <div>
+import './checkout.style.scss'
+
+
+const Checkout = () => {
+    const {cartItems,total} = useContext(CartContext);
+
+    return (
+    <div className='checkout-container'>
+        <div className='checkout-header'>
+            <div className='header-block'>
+                <span>Product</span>
+            </div>
+
+            <div className='header-block'>
+                <span>Description</span>
+            </div>
+
+            <div className='header-block'>
+                <span>Quantity</span>
+            </div>
+
+            <div className='header-block'>
+                <span>Price</span>
+            </div>
+
+            <div className='header-block'>
+                <span>Remove</span>
+            </div>
+            
+
+        </div>
+     
             {
                 cartItems.map((cartItem)=>{
-                    const {name, quantity,id} = cartItem;
-                    return <div key={id}>
-                        <h2>{name}</h2>
-                        <span>{quantity}</span>
-                        <span onClick={() => {removeItemFromCart(cartItem)}}>decrement</span>
-                        <span onClick={ () => {addItemToCart(cartItem)}}>increment</span>
-                        </div>
+                    return(
+                        <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+                    )
                 })
             }
-        </div>
+            <span className='total'>Total: ${total}</span>
     </div>
+    )
 }
 
 export default Checkout;
