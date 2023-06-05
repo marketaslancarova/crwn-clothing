b417bc772cd6b8ebf9e1094a037d08d30cb03cbf
Markéta Šlancarová <marketaslancarova@gmail.com>
2023-06-05 14:11:38 +0200

    deploy to Netlify

---
+++ crwn-clothing/apiKeys.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAwe0vrTQj1nvSfI1f2j3jZTnhiQqmz8GQ",
+    authDomain: "crwn-clothing-bb784.firebaseapp.com",
+    projectId: "crwn-clothing-bb784",
+    storageBucket: "crwn-clothing-bb784.appspot.com",
+    messagingSenderId: "932884419832",
+    appId: "1:932884419832:web:57c2be8a1f4f682a8ad70d"
+  };
+
+  export default firebaseConfig;
--- crwn-clothing/src/utils/firebase.utils.js
+++ crwn-clothing/src/utils/firebase.utils.js
@@ -6,16 +6,9 @@ import { initializeApp } from "firebase/app";
 
 import {getAuth, signInWithRedirect,signInWithPopup, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword,signOut,onAuthStateChanged } from 'firebase/auth';
 import {getFirestore,doc,getDoc,setDoc, collection, WriteBatch, writeBatch,query, getDocs} from 'firebase/firestore';
-
+import firebaseConfig from "../../../apiKeys";
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAwe0vrTQj1nvSfI1f2j3jZTnhiQqmz8GQ",
-  authDomain: "crwn-clothing-bb784.firebaseapp.com",
-  projectId: "crwn-clothing-bb784",
-  storageBucket: "crwn-clothing-bb784.appspot.com",
-  messagingSenderId: "932884419832",
-  appId: "1:932884419832:web:57c2be8a1f4f682a8ad70d"
-};
+
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
